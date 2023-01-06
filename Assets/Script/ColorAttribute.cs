efdfc48f75b99063c38f93dc6844b153b5b7e075
Samuel Chou <samuelchou@hotmail.com.tw>
2023-01-06 16:58:29 +0800

    feat(player): 新增變色器。現在會隨機變色了！

---
+++ Assets/Script/ColorAttribute.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorAttribute : MonoBehaviour
+{
+    public Color[] colors;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetRandomColor();
+    }
+
+    void SetRandomColor()
+    {
+        int random = Random.Range(0, colors.Length);
+        SetColor(colors[random]);
+    }
+
+    void SetColor(Color color)
+    {
+        spriteRenderer.color = color;
+        Debug.Log("Color set to " + color);
+    }
+}
